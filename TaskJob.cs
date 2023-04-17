670f00b1cc4aac11928d68c5839364d4591fb717
Justin Grote <JustinGrote@users.noreply.github.com>
2023-04-17 13:14:03 -0700

    Apply Formatting

--- TaskJob.cs
+++ TaskJob.cs
@@ -6,30 +6,30 @@ namespace TaskJob;
 [OutputType(typeof(TaskJob))]
 public sealed class ConvertToTaskJobCommand : PSCmdlet
 {
-    [Parameter(Mandatory = true, ValueFromPipeline = true)]
-    [ValidateNotNullOrEmpty]
-    public Task? Task;
+  [Parameter(Mandatory = true, ValueFromPipeline = true)]
+  [ValidateNotNullOrEmpty]
+  public Task? Task;
 
-    [Parameter()]
-    [ValidateNotNullOrEmpty]
-    public string? Name;
+  [Parameter()]
+  [ValidateNotNullOrEmpty]
+  public string? Name;
 
-	[Parameter()]
-	public string? Command;
+  [Parameter()]
+  public string? Command;
 
-	[Parameter()]
-	public string? Location;
+  [Parameter()]
+  public string? Location;
 
-	protected override void ProcessRecord()
-    {
-        // Task should never be null due to mandatory parameter property
-        if (Task is null)
-        { throw new ArgumentNullException(nameof(Task)); }
+  protected override void ProcessRecord()
+  {
+    // Task should never be null due to mandatory parameter property
+    if (Task is null)
+    { throw new ArgumentNullException(nameof(Task)); }
 
-        Name ??= "TaskJob" + Task.Id.ToString();
-		TaskJob taskJob = new(Task, this, Name, Command, Location);
-        WriteObject(taskJob);
-    }
+    Name ??= "TaskJob" + Task.Id.ToString();
+    TaskJob taskJob = new(Task, this, Name, Command, Location);
+    WriteObject(taskJob);
+  }
 }
 
 /// <summary>
@@ -37,81 +37,74 @@ public sealed class ConvertToTaskJobCommand : PSCmdlet
 /// </summary>
 public sealed class TaskJob : Job2
 {
-	readonly Task task;
-	readonly Job2 managedJob;
-	readonly string location;
-	public TaskJob(Task Task, PSCmdlet Cmdlet, string? Name, string? Command, string? Location) : base(Command, Name)
-    {
-        PSJobTypeName = "TaskJob";
-        this.Name = Name ?? "Task" + Task.Id;
-		task = Task;
-		Location ??= "task";
-		location = Location;
+  readonly Task Task;
+  readonly Job2 ManagedJob;
+  public override bool HasMoreData => Output.Count > 0;
+  public override string Location { get; }
+  public override string StatusMessage => Task.Status.ToString();
+  public TaskJob(Task task, PSCmdlet cmdlet, string? name, string? command, string? location) : base(command, name)
+  {
+    PSJobTypeName = "TaskJob";
+    Name = name ?? "Task" + task.Id;
+    Task = task;
+    Location = location ?? "task";
 
-		Output.EnumeratorNeverBlocks = true;
+    Output.EnumeratorNeverBlocks = true;
 
-		// Create a job definition and register it with the PowerShell job manager so it can be seen via Get-Job.
-		// The labels here don't seem to matter or show up anywhere
-		JobDefinition jobDefinition = new(typeof(TaskJobSourceAdapter), Command, Name);
-        Dictionary<string, object> parameterCollection = new()
+    // Create a job definition and register it with the PowerShell job manager so it can be seen via Get-Job.
+    // The labels here don't seem to matter or show up anywhere
+    JobDefinition jobDefinition = new(typeof(TaskJobSourceAdapter), command, name);
+    Dictionary<string, object> parameterCollection = new()
         {
             { "NewJob", this }
         };
-        JobInvocationInfo jobSpecification = new(jobDefinition, parameterCollection);
-		managedJob = Cmdlet.JobManager.NewJob(jobSpecification);
-		task.Wait();
-        dynamic result = ((dynamic)Task).Result;
-		Output.Add(result);
-		SetJobState(JobState.Completed);
-	}
+    JobInvocationInfo jobSpecification = new(jobDefinition, parameterCollection);
+    ManagedJob = cmdlet.JobManager.NewJob(jobSpecification);
+    Task.Wait();
+    dynamic result = ((dynamic)task).Result;
+    Output.Add(result);
+    SetJobState(JobState.Completed);
+  }
 
-    public override bool HasMoreData => Output.Count > 0;
+  public override void ResumeJobAsync()
+  {
+    throw new NotSupportedException("Tasks do not natively support being suspended or resumed");
+  }
+  public override void ResumeJob() { ResumeJob(); }
 
-	public override string Location => location;
+  // Tasks start automatically, we don't need to do it here
+  public override void StartJob() { }
 
-	public override string StatusMessage => task.Status.ToString();
+  public override void StartJobAsync() { }
 
+  public override void StopJob(bool force, string reason)
+  {
+    throw new NotSupportedException("Tasks cannot be cancelled directly, you must run Cancel() on a tokenSource");
+  }
 
+  public override void StopJob() { StopJob(true, string.Empty); }
 
-	public override void ResumeJobAsync()
-	{
-		throw new NotSupportedException("Tasks do not natively support being suspended or resumed");
-	}
-	public override void ResumeJob() { ResumeJob(); }
+  public override void StopJobAsync() { StopJob(); }
 
-	// Tasks start automatically, we don't need to do it here
-	public override void StartJob() { }
+  public override void StopJobAsync(bool force, string reason) { StopJob(force, reason); }
 
-	public override void StartJobAsync() { }
+  public override void SuspendJob(bool force, string reason)
+  {
+    throw new NotSupportedException("Tasks do not natively support being suspended or resumed");
+  }
 
-    public override void StopJob(bool force, string reason)
-    {
-		throw new NotSupportedException("Tasks cannot be cancelled directly, you must run Cancel() on a tokenSource");
-    }
+  public override void SuspendJob() { SuspendJobAsync(); }
 
-	public override void StopJob() { StopJob(true, string.Empty); }
+  public override void SuspendJobAsync() { SuspendJob(true, string.Empty); }
 
-	public override void StopJobAsync() { StopJob(); }
+  public override void SuspendJobAsync(bool force, string reason) { SuspendJob(force, reason); }
 
-	public override void StopJobAsync(bool force, string reason) { StopJob(force, reason); }
+  public override void UnblockJob()
+  {
+    throw new NotSupportedException("Tasks do not natively support being blocked or unblocked");
+  }
 
-	public override void SuspendJob(bool force, string reason)
-	{
-		throw new NotSupportedException("Tasks do not natively support being suspended or resumed");
-	}
-
-	public override void SuspendJob() { SuspendJobAsync(); }
-
-	public override void SuspendJobAsync() { SuspendJob(true, string.Empty); }
-
-	public override void SuspendJobAsync(bool force, string reason) { SuspendJob(force, reason); }
-
-    public override void UnblockJob()
-    {
-		throw new NotSupportedException("Tasks do not natively support being blocked or unblocked");
-    }
-
-	public override void UnblockJobAsync() { UnblockJob(); }
+  public override void UnblockJobAsync() { UnblockJob(); }
 }
 
 /// <summary>
@@ -119,57 +112,57 @@ public sealed class TaskJob : Job2
 /// </summary>
 public sealed class TaskJobSourceAdapter : JobSourceAdapter
 {
-    private readonly ConcurrentDictionary<Guid, Job2> jobs = new();
-    public override Job2 GetJobByInstanceId(Guid instanceId, bool recurse)
-    {
-		return jobs[instanceId];
-	}
+  private readonly ConcurrentDictionary<Guid, Job2> jobs = new();
+  public override Job2 GetJobByInstanceId(Guid instanceId, bool recurse)
+  {
+    return jobs[instanceId];
+  }
 
-    public override Job2 GetJobBySessionId(int id, bool recurse)
-    {
-		return jobs.Values.Where(job => job.Id.Equals(id)).Single();
-	}
+  public override Job2 GetJobBySessionId(int id, bool recurse)
+  {
+    return jobs.Values.Single(job => job.Id.Equals(id));
+  }
 
-    public override IList<Job2> GetJobsByCommand(string command, bool recurse)
-    {
-		return jobs.Values.Where(job => job.Command.Equals(command)).ToList();
-	}
+  public override IList<Job2> GetJobsByCommand(string command, bool recurse)
+  {
+    return jobs.Values.Where(job => job.Command.Equals(command)).ToList();
+  }
 
-    public override IList<Job2> GetJobsByFilter(Dictionary<string, object> filter, bool recurse)
+  public override IList<Job2> GetJobsByFilter(Dictionary<string, object> filter, bool recurse)
+  {
+    throw new NotImplementedException("The TaskJobSourceAdapter has no adapter-specific filters");
+  }
+
+  public override IList<Job2> GetJobsByName(string name, bool recurse)
+  {
+    return jobs.Values.Where(job => job.Name.Equals(name)).ToList();
+  }
+
+  public override IList<Job2> GetJobsByState(JobState state, bool recurse)
+  {
+    return jobs.Values.Where(job => job.JobStateInfo.State.Equals(state)).ToList();
+  }
+
+  public override Job2 NewJob(JobInvocationInfo specification)
+  {
+    if (specification.Parameters[0][0].Value is not TaskJob job)
     {
-		throw new NotImplementedException("The TaskJobSourceAdapter has no adapter-specific filters");
+      throw new InvalidDataException("No ThreadJob found in the JobSpecification parameters. This is probably a bug");
     }
+    jobs.TryAdd(job.InstanceId, job);
+    return job;
+  }
 
-    public override IList<Job2> GetJobsByName(string name, bool recurse)
+  public override void RemoveJob(Job2 job)
+  {
+    if (!jobs.TryRemove(job.InstanceId, out _))
     {
-		return jobs.Values.Where(job => job.Name.Equals(name)).ToList();
+      throw new InvalidOperationException($"Job {job.Name} ({job.InstanceId}) does not exist in the catalog");
     }
+  }
 
-    public override IList<Job2> GetJobsByState(JobState state, bool recurse)
-    {
-		return jobs.Values.Where(job => job.JobStateInfo.State.Equals(state)).ToList();
-    }
-
-    public override Job2 NewJob(JobInvocationInfo specification)
-    {
-        if (specification.Parameters[0][0].Value is not TaskJob job)
-        {
-            throw new InvalidDataException("No ThreadJob found in the JobSpecification parameters. This is probably a bug");
-        }
-        jobs.TryAdd(job.InstanceId, job);
-        return job;
-    }
-
-    public override void RemoveJob(Job2 job)
-    {
-		if (!jobs.TryRemove(job.InstanceId, out _))
-		{
-			throw new InvalidOperationException($"Job {job.Name} ({job.InstanceId}) does not exist in the catalog");
-		}
-	}
-
-    public override IList<Job2> GetJobs()
-    {
-        return jobs.Values.ToList();
-    }
+  public override IList<Job2> GetJobs()
+  {
+    return jobs.Values.ToList();
+  }
 }
