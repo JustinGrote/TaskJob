d011b6c534fc53c3081829acb4f9d506b4f3aa35
Justin Grote <JustinGrote@users.noreply.github.com>
2023-04-19 09:08:38 -0700

    Refactor for netstandard 2.0 (remove implicit usings, etc.)

--- TaskJob.cs
+++ TaskJob.cs
@@ -1,5 +1,11 @@
-﻿using System.Collections.Concurrent;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Management.Automation;
+using System.Threading.Tasks;
+
 namespace TaskJob;
 
 [Cmdlet(VerbsData.ConvertTo, "TaskJob")]
